fc267360e374128a76990d32aafd2b8610940257
Sofia Ayala <sofiarayala1995@gmail.com>
2025-04-30 13:35:52 +0200

    added budgetList interface and @for

--- src/app/components/budgets-list/budgets-list.component.ts
+++ src/app/components/budgets-list/budgets-list.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
-
+import { BudgetList } from '../../interface/budget-list';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-budgets-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './budgets-list.component.html',
-  styleUrl: './budgets-list.component.scss'
+  styleUrl: './budgets-list.component.scss',
 })
 export class BudgetsListComponent {
-
+  budgetList: BudgetList[] = [
+    {
+      title: 'SEO',
+      description: 'Programació de una web responsive completa',
+      price: 300,
+    },
+    {
+      title: 'Advertising',
+      description: 'Programació de una web responsive completa',
+      price: 400,
+    },
+    {
+      title: 'Website Development',
+      description: 'Programació de una web responsive completa',
+      price: 500,
+    },
+  ];
 }
---
+++ src/app/interface/budget-list.ts
@@ -0,0 +1,5 @@
+export interface BudgetList {
+  title: string;
+  description: string;
+  price: number;
+}
