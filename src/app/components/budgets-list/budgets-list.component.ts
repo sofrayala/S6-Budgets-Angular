5fe037f18454c97a4fe3d1d9e8fef4b8375ce38a
Sofia Ayala <sofiarayala1995@gmail.com>
2025-05-02 14:53:13 +0200

    added calculateTotalBudget() function and added forms to the budget-list-component

--- src/app/components/budgets-list/budgets-list.component.ts
+++ src/app/components/budgets-list/budgets-list.component.ts
@@ -1,28 +1,58 @@
 import { Component } from '@angular/core';
-import { BudgetList } from '../../interface/budget-list';
+import { BudgetList } from '../../interface/budget-list.interface';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormBuilder,
+  FormGroup,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-budgets-list',
-  imports: [CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './budgets-list.component.html',
   styleUrl: './budgets-list.component.scss',
 })
 export class BudgetsListComponent {
-  budgetList: BudgetList[] = [
+  budgetForm: FormGroup;
+  totalBudget: number = 0;
+
+  budgetList = [
     {
       title: 'SEO',
       description: 'Program a complete responsive web',
       price: 300,
+      controlName: 'seo',
     },
     {
       title: 'Advertising',
       description: 'Program a complete responsive web',
       price: 400,
+      controlName: 'advertising',
     },
     {
       title: 'Website Development',
       description: 'Program a complete responsive web',
       price: 500,
+      controlName: 'website',
     },
   ];
+
+  constructor(private fb: FormBuilder) {
+    this.budgetForm = this.fb.group({
+      seo: false,
+      advertising: false,
+      website: false,
+    });
+
+    this.budgetForm.valueChanges.subscribe((values) => {
+      this.calculateTotalBudget(values);
+    });
+  }
+
+  calculateTotalBudget(values: any): void {
+    this.totalBudget = this.budgetList.reduce((total, service) => {
+      return total + (values[service.controlName] ? service.price : 0);
+    }, 0);
+  }
 }
