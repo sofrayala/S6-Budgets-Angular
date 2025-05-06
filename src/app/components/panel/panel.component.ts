81b21c4712668d15d55999434c9450fc8b10b817
Sofia Ayala <sofiarayala1995@gmail.com>
2025-05-06 13:51:00 +0200

    implemented reactive forms for buttons +/- (pages & language)

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 @Component({
   selector: 'app-panel',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './panel.component.html',
-  styleUrl: './panel.component.scss'
+  styleUrl: './panel.component.scss',
 })
 export class PanelComponent {
+  @Output() websitePricePersonalized = new EventEmitter<number>();
+  panelForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.panelForm = this.fb.group({
+      pages: [1, [Validators.required, Validators.min(1)]],
+      languages: [1, [Validators.required, Validators.min(1)]],
+    });
+
+    this.panelForm.valueChanges.subscribe((values) => {
+      const price = this.calculateWebsitePrice(values.pages, values.languages);
+      this.websitePricePersonalized.emit(price);
+    });
+  }
+
+  calculateWebsitePrice(pages: number, languages: number): number {
+    return pages * languages * 30;
+  }
+
+  addPages(): void {
+    const currentPages = this.panelForm.get('pages')?.value || 1;
+    this.panelForm.get('pages')?.setValue(currentPages + 1);
+  }
+
+  substractPages(): void {
+    const currentPages = this.panelForm.get('pages')?.value || 1;
+    if (currentPages > 1) {
+      this.panelForm.get('pages')?.setValue(currentPages - 1);
+    }
+  }
+
+  addLanguages(): void {
+    const currentLanguages = this.panelForm.get('languages')?.value || 1;
+    this.panelForm.get('languages')?.setValue(currentLanguages + 1);
+  }
+
+  substractLanguages(): void {
+    const currentLanguages = this.panelForm.get('languages')?.value || 1;
+    if (currentLanguages > 1) {
+      this.panelForm.get('languages')?.setValue(currentLanguages - 1);
+    }
+  }
 }
