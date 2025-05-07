2f1bda9bf2e5ccd7263b4633a54eb3523d9c7005
Sofia Ayala <sofiarayala1995@gmail.com>
2025-05-07 13:29:17 +0200

    implemented unir test for the calculateWebsitePrice() fucntion

--- src/app/services/budget.service.spec.ts
+++ src/app/services/budget.service.spec.ts
@@ -13,4 +13,14 @@ describe('BudgetService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should calculate website panel price correctly', () => {
+    const pages = 2;
+    const languages = 3;
+    const expectedPrice = pages * languages * 30;
+
+    const result = service.calculateWebsitePrice(pages, languages);
+
+    expect(result).toBe(expectedPrice);
+  });
 });
