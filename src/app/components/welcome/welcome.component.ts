c38089210f9d877dc9f0bbe36d5dedf4b0cafea1
Sofia Ayala <sofiarayala1995@gmail.com>
2025-05-12 10:43:58 +0200

    implemented routing to welcome component

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [RouterModule, RouterLink],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrl: './welcome.component.scss',
 })
-export class WelcomeComponent {
-
-}
+export class WelcomeComponent {}
